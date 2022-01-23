4ebafbdb8c91a83c9b0f0535bc20ab6672f621dc
stroemlind <elin.stromlind@gmail.com>
2022-01-23 19:45:50 +0000

    Fix typo

--- codestar/settings.py
+++ codestar/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'codestar.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATES_DIR],
+        'DIRS': [TEMPLATES],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
